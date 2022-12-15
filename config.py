bf01c9b0a9e26c1028f8e2c3fea9c66125f48a3d
Lesh1i <76016418+Lesh1i@users.noreply.github.com>
2022-12-15 09:55:39 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+# API_TOKEN
+API_TOKEN ='5750165762:AAHXTubTqTwAVsfruQNpzzInlPQuVUr7f1s'
+
+#Admin ID
+admin_id = 888881913
+
+#Database connection settings
+dbHost = 'sql.freedb.tech'
+dbUser= 'freedb_KomarovKrasyuk'
+dbPassword = 'k3Nz6x&7&#JejZZ'
+dbName = 'freedb_DatabaseKomarovKrasyuk'
